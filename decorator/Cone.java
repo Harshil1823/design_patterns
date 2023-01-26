65d8e33beb8dc3a26fca2560ca904141439a1bd6
Harshil Shah <harshilshah@Harshils-MBP.lan>
2023-01-25 22:54:12 -0500

    fixed errors in Cone class also added output.png

--- decorator/Cone.java
+++ decorator/Cone.java
@@ -4,9 +4,19 @@ public class Cone extends IceCream {
 
     private ConeType coneType;
 
+   
     public Cone (ConeType coneType){
         this.coneType = coneType;
-
+        if(coneType == coneType.SUGAR_CONE){
+            this.description = "Sugar cone";
+        }else if(coneType == coneType.WAFFLE_CONE){
+            this.description = "Waffle cone";
+        }else if(coneType == coneType.PRETZEL_CONE){
+            this.description = "Pretzel cone";
+        }else if(coneType == coneType.CHOCOLATE_DIPPED_CONE){
+            this.description = "Chocolate dipped Cone";
+        }
+        /* 
         if(coneType.equals("SUGAR_CONE")){
             this.description = "Sugar cone";
         }else if (coneType.equals("WAFFLE_CONE")){
@@ -14,13 +24,26 @@ public class Cone extends IceCream {
         }else if (coneType.equals("PRETZEL CONE")){
             this.description = "Pretzel cone";
         }else if (coneType.equals("CHOCOLATE_DIPPED_CONE")){
-            this.description = "Chocolate Dipped Cone";
+            this.description = "Chocolate dipped Cone";
         }
-        
+        */
     }
     public double getCost() {
 
         double tempNum = 0.0;
+        
+        if(coneType == coneType.SUGAR_CONE){
+            return tempNum = tempNum + .75;
+        }else if(coneType == coneType.WAFFLE_CONE){
+            return tempNum = tempNum + 1.2;
+        }else if(coneType == coneType.PRETZEL_CONE){
+            return tempNum = tempNum + 1.8;
+        }else if(coneType == coneType.CHOCOLATE_DIPPED_CONE){
+            return tempNum = tempNum + 1.5;
+        }else{
+            return tempNum;
+        }
+        /* 
         if(coneType.equals("SUGAR_CONE")){
             return tempNum = tempNum + .75;
         }else if (coneType.equals("WAFFLE_CONE")){
@@ -32,6 +55,7 @@ public class Cone extends IceCream {
         }else{
             return tempNum;
         }
+        */
     }
     
 }
