ed79b1b176576066af17013cdc0136c39a9de57d
Harshil Shah <harshilshah@Harshils-MBP.lan>
2023-04-20 21:26:10 -0400

    fixed bugs

--- adapter/MediaAdapter.java
+++ adapter/MediaAdapter.java
@@ -1,5 +1,6 @@
 package adapter;
 
+import java.security.spec.NamedParameterSpec;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,27 +9,42 @@ import javax.imageio.plugins.tiff.BaselineTIFFTagSet;
 
 public class MediaAdapter implements Media {
     private LittleMedia littleMedia;
-
+    /**
+     * Basically sets the littleMedia to
+     * littleMedia that's passes in as a object
+     * @param littleMedia
+     */
     public MediaAdapter(LittleMedia littleMedia){
         this.littleMedia = littleMedia;
     }
-
+    /**
+     * returns String of the title 
+     */
     @Override
     public String getTitle() {
         return littleMedia.getTitle();
     }
-
+    /**
+     * returns the object author which it gets from 
+     * Little media getAuthor method
+     * it manipules strings and that's it
+     */
     @Override
     public Author getAuthor() {
         String [] names = littleMedia.getAuthor().split(" ");
         return new Author(names[0], names[1]);
     }
-
+    /**
+     * returns getDescrption as a String
+     */
     @Override
     public String getDescription() {
         return littleMedia.getDescription();
     }
-
+    /**
+     * Takes the necessary params
+     * and it organizes them 
+     */
     @Override
     public void addReview(String firstName, String lastName, Date date, double rating, String comment) {
         String tempName = firstName + " " + lastName;
@@ -37,25 +53,25 @@ public class MediaAdapter implements Media {
 
     @Override
     public ArrayList<Review> getReviews() {
-        ArrayList<Review> reviews = new ArrayList<>();
+        ArrayList<Review> allReviews = new ArrayList<Review>();
+        ArrayList<Review> reviews = new ArrayList<Review>();
         for(String review : littleMedia.getReviews()) {
             String [] parts = review.split(" - ");
-            String [] names = parts[1].split(" ");
-            Author author = new Author(names[0], names[1]);
-            String dateString = parts[2].substring(parts[2].lastIndexOf(" ") + 1);
-            Date date = new Date();
-            try {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("MM/DD/YYYY");
-                date = dateFormat.parse(dateString);
-            } catch (Exception e) {
-                e.printStackTrace();
+            double rating = Double.parseDouble(parts[0].split(" ")[0]);
+            String comment = parts[1];
+            String [] names = parts[2].split(" ");
+            String firstName = names[0];
+            String lastName = "";
+            // addding a check statement 
+            if(names.length > 1) {
+                lastName = names[1];
             }
-            double rating = Double.parseDouble(parts[0].substring(0, parts[0].indexOf(" ")));
-            String comment = parts[0].substring(parts[0].indexOf(" ") + 1);
-            Review reviewObj = new Review(author.getFirstName(), author.getLastName(), date, rating, comment);
-            reviews.add(reviewObj); 
+            Date date = new Date();
+            allReviews.add(new Review(firstName, lastName, date, rating, comment));
         }
-        return reviews;
+        for (Review review : reviews) {
+            allReviews.add(review);
+        }
+        return allReviews;
     }
-
 }
